5b1630813b54841abfead8c150acef3fe03b895e
Egor <aldes@list.ru>
2022-12-02 22:18:12 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.Write("Ура, это же Маша");
+
+}
+else
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
